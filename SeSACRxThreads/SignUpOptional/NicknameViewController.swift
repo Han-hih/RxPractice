44f09af9283b68b61fcef97f79bf7e49c0eb1fe7
한 <hih458@naver.com>
2023-11-01 20:09:16 +0900

    feat 닉네임 2글자이상 6글자 미만일 때 버튼 hidden설정

--- SeSACRxThreads/SignUpOptional/NicknameViewController.swift
+++ SeSACRxThreads/SignUpOptional/NicknameViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class NicknameViewController: UIViewController {
    
     let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let disposeBag = DisposeBag()
     
+    let buttonHidden = BehaviorSubject(value: false)
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = Color.white
         
         configureLayout()
-       
+       bind()
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
 
     }
     
+    func bind() {
+        buttonHidden
+            .bind(to: nextButton.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        nicknameTextField.rx.text.map { $0!.count < 2 || $0!.count >= 6 }
+            .subscribe(with: self) { owner, value in
+                let isHidden = value ? true : false
+                owner.buttonHidden.onNext(value)
+            }
+            .disposed(by: disposeBag)
+            
+        
+        
+    }
     @objc func nextButtonClicked() {
         navigationController?.pushViewController(BirthdayViewController(), animated: true)
     }
